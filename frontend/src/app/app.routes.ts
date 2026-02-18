05892e02af3548033f55f3c452221fa3bda7bdd6
Joao Vitor Nardi <joaonardiuitec@gmail.com>
2026-02-18 15:01:24 -0300

    Arquitetura de layout header navbar e footers (vazios)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+      //proximas paginas
+    ]
   }
 ];
---
+++ frontend/src/app/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  template: `
+    <app-header />
+    <main>
+      <router-outlet />
+    </main>
+    <app-footer />
+  `
+})
+export class LayoutComponent {}
